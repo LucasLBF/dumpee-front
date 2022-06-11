a42e2f9ae1a4fd2cacf200e62f06e62d77ada0ad
LucasLBF <lucas.fernandes1454@gmail.com>
2022-06-11 14:43:51 -0300

    complete first version of welcome screen

--- components/WelcomeScreen/WelcomeScreen.tsx
+++ components/WelcomeScreen/WelcomeScreen.tsx
@@ -1,13 +1,67 @@
-import { Text, View } from "react-native";
+import { Text, View, Pressable } from "react-native";
 import { useTheme } from "react-native-paper";
 import makeStyles from "./WelcomeScreenStyles";
+import * as SplashScreen from "expo-splash-screen";
+import * as Font from "expo-font";
+import { useState, useEffect, useCallback } from "react";
 
 const WelcomeScreen = () => {
   const theme = useTheme();
   const styles = makeStyles(theme);
+  const [appIsReady, setAppIsReady] = useState(false);
+
+  useEffect(() => {
+    async function prepare() {
+      try {
+        await SplashScreen.preventAutoHideAsync();
+        // Pre-load fonts, make any API calls you need to do here
+        await Font.loadAsync({
+          Poppins: require("../../assets/fonts/Poppins-Medium.ttf"),
+          PoppinsSemi: require("../../assets/fonts/Poppins-SemiBold.ttf"),
+          Roboto: require("../../assets/fonts/Roboto-Medium.ttf"),
+        });
+      } catch (e) {
+        console.warn(e);
+      } finally {
+        // Tell the application to render
+        setAppIsReady(true);
+      }
+    }
+
+    prepare();
+  }, []);
+
+  const onLayoutRootView = useCallback(async () => {
+    if (appIsReady) {
+      await SplashScreen.hideAsync();
+    }
+  }, [appIsReady]);
+
+  if (!appIsReady) {
+    return null;
+  }
+
   return (
-    <View style={styles.background}>
-      <Text>Dumpee</Text>
+    <View style={styles.background} onLayout={onLayoutRootView}>
+      <View style={styles.titleContainer}>
+        <Text style={styles.title}>Dumpee</Text>
+      </View>
+      <View style={styles.imgContainer}></View>
+      <View style={styles.bottomContainer}>
+        <Text style={styles.welcomeTitle}>Bem-Vinde!</Text>
+        <View style={styles.welcomeTextContainer}>
+          <Text style={styles.welcomeText}>
+            Dê o destino correto aos eletroeletronicos que não usa mais, ajude o
+            planeta e ainda ganhe prêmios!
+          </Text>
+        </View>
+        <Pressable
+          style={styles.startButtonContainer}
+          onPress={() => console.log("next screen")}
+        >
+          <Text style={styles.startButtonText}>Entrar</Text>
+        </Pressable>
+      </View>
     </View>
   );
 };
--- components/WelcomeScreen/WelcomeScreenStyles.tsx
+++ components/WelcomeScreen/WelcomeScreenStyles.tsx
@@ -1,14 +1,54 @@
 import { StyleSheet } from "react-native";
 
-const makeStyles = (themeProps: ReactNativePaper.Theme) =>
-  StyleSheet.create({
+const makeStyles = (themeProps: ReactNativePaper.Theme) => {
+  return StyleSheet.create({
     background: {
       flex: 1,
       alignItems: "center",
       justifyContent: "center",
       width: "100%",
+      padding: 16,
       backgroundColor: themeProps.colors.primary,
     },
+    titleContainer: { flex: 1, justifyContent: "flex-end" },
+    title: { fontFamily: "Poppins", fontSize: 50, color: "#E3F2DC" },
+    imgContainer: { flex: 2 },
+    bottomContainer: {
+      flex: 3,
+      alignItems: "center",
+      width: "100%",
+    },
+    welcomeTitle: {
+      fontFamily: "PoppinsSemi",
+      fontSize: 40,
+      color: "#FFF1CD",
+      marginBottom: 20,
+    },
+    welcomeTextContainer: {
+      paddingHorizontal: 64,
+      marginBottom: 56,
+    },
+    welcomeText: {
+      fontFamily: "Roboto",
+      fontSize: 14,
+      color: "#FFF1CD",
+      textAlign: "center",
+    },
+    startButtonContainer: {
+      justifyContent: "center",
+      width: "100%",
+      height: 50,
+      backgroundColor: "#FFF1CD",
+      borderRadius: 6,
+    },
+    startButtonText: {
+      textAlign: "center",
+      fontFamily: "Roboto",
+      fontWeight: "bold",
+      fontSize: 14,
+      color: "#335D42",
+    },
   });
+};
 
 export default makeStyles;
